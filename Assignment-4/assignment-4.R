1a1ec1ce1257bd762b6974c958fcc2a0defb6190
Angelica Patino <angelica_patino@brown.edu>
2026-04-10 09:34:57 -0400

    load shiny package

---
+++ Assignment-4/assignment-4.R
@@ -0,0 +1,6 @@
+# The lines below are run if the packages were not yet installed.
+# Otherwise,they are commented out so the code is not executed.
+# install.packages("shiny")
+
+# The shiny package enables the use of the shiny app utilities.
+library(shiny)
