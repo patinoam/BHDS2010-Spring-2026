d7e97a0d3b892bb2c44b407105ed335dd2997fb7
Angelica Patino <angelica_patino@brown.edu>
2026-04-13 03:39:38 -0400

    data wrangling for cleaning up dates, location ids, state and county, and variable types

--- Assignment-4/app.R
+++ Assignment-4/app.R
@@ -1,3 +1,8 @@
+# TODO remove below when for app deploy; in read.csv, replace path with filename only
+wd_path <- getwd()
+data_path <- paste0(wd_path,
+                    "/Assignment-4/measles_county_all_updates_detailed.csv")
+                    
 #####
 ##### Install and load packages
 #####
@@ -26,3 +31,39 @@ library(lubridate)
 # The tidyverse package contains functions for data wrangling and 
 # generating figures, such as `ggplot`, among others.
 library(tidyverse)
+
+#####
+##### Data loading and preparation
+#####
+
+# The data file is read into the R environment using read_csv from the readr
+# package. An object called raw is created which contains the imported data.
+raw <- read.csv(data_path, header=TRUE) %>%
+  mutate(
+    # The date column is converted from a character string to an R Date object
+    # so that date-based filtering and arithmetic can be performed.
+    date = as.Date(date),
+    
+    # The location_id column is zero-padded to 5 digits to match the format
+    # of US Census FIPS codes used by the tigris shapefiles.
+    # For example, 8001 becomes "08001" for Adams County, Colorado.
+    fips = sprintf("%05d", as.integer(location_id)),
+    
+    # The state name is extracted from the location_name column, which is
+    # formatted as "County, State". The sub function removes everything up to
+    # and including the comma and space. trimws removes any remaining whitespace.
+    state = trimws(sub(".*,\\s*", "", location_name)),
+    
+    # The county name is extracted from the location_name column by removing
+    # everything from the comma onward.
+    county = trimws(sub(",.*", "", location_name)),
+    
+    # The value column is explicitly converted to numeric to ensure case counts
+    # are stored as numbers and not character strings.
+    value = as.numeric(value),
+    
+    # Each date is rounded down to the start of its week using floor_date.
+    # For example, a Wednesday date becomes the preceding Sunday.
+    # This column is used for grouping in the weekly case count chart.
+    week = floor_date(date, "week")
+  )
